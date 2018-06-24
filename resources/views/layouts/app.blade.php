44c710ad85fca0c940629a314868c6215218f795
zarem <zarem@192.168.1.106>
2018-06-24 15:05:13 +0200

    added graph js library

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,5 +15,6 @@
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/diceroller.js') }}"></script>
 	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
     </body>
 </html>
